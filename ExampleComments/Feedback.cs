7ceb0b0194fc3e412305feb0c4c7950fac69d270
OJacot-Descombes <ojd@cysoft.ch>
2023-07-10 13:01:59 +0200

    Added examples.

--- ExampleComments/Feedback.cs
+++ ExampleComments/Feedback.cs
@@ -1,4 +1,5 @@
 ﻿// https://marketplace.visualstudio.com/items?itemName=OlivierJacot-Descombes.PrettyDocComments&ssr=false#qna
+/// <h4>Custom Tags</h4>
 /// <internalNotes note="Place comments to exclude from documentation here">
 ///  <para>
 ///   The <see langword="static"/> <see cref="M:Program.Main(string[])"/> method
@@ -13,4 +14,10 @@
 /// <initialAuthoringDate>
 ///  7/8/2023
 /// </initialAuthoringDate>
+/// <br/>
+/// <br/>
+/// <h4>Mark Tag</h4>
+/// <mark  style="color: green;">Marked text</mark>
+/// <mark style="background: white;">Marked text</mark>
+/// <mark style="background: #00ced1!important">Marked text</mark>
 internal class CustomTagsBy_K_L_Carter_Sr{ }
